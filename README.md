98ade01164652284608190d6500a3b6e52ea9e9f
pok49 <sergio.pokrovskij@gmail.com>
2025-03-13 12:15:11 +0700

    the 1st substatial commit

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# eomacs
-Adapting Emacs for Esperanto (eo)
+# eomacs: Adapting Emacs for Esperanto (eo)
+
+##  e8: A Quail package for inputting Esperanto and more
+This is a combination of 2 input metods:
+
+1. A direct method to input Esperanto letters; e.g. a `QWERTYUIOP[]`
+   keyboard is given the `ŜŬERTĜUIOPĴĤ` layout. (This method is also
+   applicable to many other layouts as explained below.)
+2. An input method for many other characters using their [TeX](https://en.wikipedia.org/wiki/TeX)-ish names
+   or [Compose sequences](https://en.wikipedia.org/wiki/Compose_key) in the X System of Unix.
+
+The historic name “e8” is a combination of “Esperanto” (e) and the encoding “UTF-8” (8).
+In the Emacs’ mode-line it is marked as **ŭ8** (**U**TF-**8** with a bowl ‹˘›
+over **u** reminiscent of an Esperantic diacritic).
+
+In both cases semicolon (;) is used as a prefix modifyer. If the
+characters folllowing a semicolon do not form a symbol name defined in
+this `e8` method, then the semicolon and the following string are
+inputted “as is”; in particular, ‘`; SPC`’ is “semicolon followed by a
+space”; so that a reliable method to input a semicolon (even before a
+string which coincides with an ``e8`` symbol name) is to type ‘`; SPC BackSpace`’.
+
+### The Direct Method
+For definiteness assume that we are using a common US QWERTY keyboard.
+
+There are 6 supersigned Esperantic letters missing in the English
+alphabet; there are 4 free English letters (QWXY) which are not used
+in Esperanto and thus can be taken for its extra letters; but 2
+remainig letters have to be taken from the variant zone, and the most
+natural candidates are the keys `D11` and `D12`, according to the
+numeration of [ISO/IEC 9995-2:2009](https://en.wikipedia.org/wiki/ISO/IEC_9995)[^1 ]:
+
+![Standard Keyboard Alphanumeric Section](doc/Keyboard-alphanumeric-section-ISOIEC-9995-2-2009-with-amd1-2012.png)
+
+In the US keyboard these are `[`∖`{` and `}`∖`]`. I use the folloing map:
+
+| Ĉ ĉ | Ĝ ĝ | Ĥ ĥ | Ĵ ĵ | Ŝ ŝ | Ŭ ŭ | ʼ |
+| X x | Y y | } ] | { [ | Q q | W w | ` |
+
+The Quail method maps character to character, not key to character.
+E.g. the **B01** key, which produces W in ASERTY, Y in QWERTZ (and Z in
+QWERTY), is mapped from these layouts to Ŭ, Ĵ (and Z), respectively.
+
+In other words, in order to input\
+   `eĥoŝanĝo ĉiuĵaŭde`   one should press the keys corresponding to\
+   `e]oqanyo xiu[awde`   in the original layout.
+
+Conversely, in order to input `quick brown fox` from e8 mode one can use
+semicolons:\
+   `;quick bro;wn fo;x,`
+or simply toggle off the e8 mode with ``C‑\``. (Besides, instead of
+semicolon you can use `C‑q` to get the original, crude value of the next key
+you press thereafter: `C‑q w` ⇒ `w`, and not `ŭ`.)
+
+### The TeX/Compose Part
+
+Originally for the input of the extra characters **e8** relied on the TeX
+definitions from `unicode-input.el` by Florian Weimer; presently these
+are being gradually replaced with the shorter and internationally more
+intuitive Compose sequences (e.g. `;->` instead of `;rightarrow`). The
+Compose sequences have the advantage of being usable in the
+applications other than Emacs; OTOH Emacs (and hence e8) is available
+on MS Windows and many other non-Unix systems.
+
+## Installation
+
+Put `e8.el` somewhere into the `quail` hierarchy, e.g.\
+`site-lisp/quail/`
+
+Add this to your `.emacs` or other initialization file:
+```lang-lisp
+(register-input-method
+ "e8" "utf-8" 'quail-use-package
+ "ŭ8" "A direct method to input Esperanto letters"
+ "e8")
+```
+After this piece is evaluated, you can\
+`(set-input-method 'e8)`
+from a script or interactivly via `C-x RET C-\`.
+
+Afterwards you can switch to another input method in a similar way, or
+`toggle-input-method` (``C‑\``) to disable or enable a multilingual text input
+method for the current buffer.
+
+
+
+[^1]: This image (CC BY-SA by [Karl432](https://commons.wikimedia.org/wiki/User:Karl432)) is taken [from Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Keyboard-alphanumeric-section-ISOIEC-9995-2-2009-with-amd1-2012.png).
+
---
+++ e8.el
@@ -0,0 +1,512 @@
+;;; quail/e8.el -- Quail package for inputting Esperanto -*- coding: utf-8 -*-
+
+;; Copyright (C) 1999 Sergio Pokrovskij
+;; Many TeX definitions are stolen from unicode-input.el by Florian Weimer
+
+;; Keywords: multilingual, input method, Esperanto, TeX
+
+;; This file is not a part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
+
+(require 'quail)
+
+(quail-define-package
+ "e8" "utf-8" "ŭ8" t
+ "A direct method to input Esperanto letters and other characters
+to UTF-8 encoding using TeX-ish names.
+
+Semicolon (;) is used as a modifyer.  In order to input
+\t“eĥoŝanĝo ĉiuĵaŭde” type
+\t“e]oqanyo xiu[awde”; in order to input either of qwyx[]{} precede
+it with semicolon.
+
+Other characters:
+    effect   | prefix | examples
+ ------------+--------+----------
+    acute    |   ;'    | ;'a -> á   '' -> ?´
+    grave    |   ;`    | ;`a -> à
+  circumflex |   ;^    | ;^a -> â
+  diaeresis  |   ;\"   | ;\"a -> ä  ;\"{} -> ¨
+   cedilla   |   ;c    | ;cc -> ç   ;c{} -> ¸
+  dot above  |   ;.    | ;.g -> ġ   ;.z -> ż   ;.I -> İ
+" nil t nil nil nil nil nil nil nil nil t)
+
+(quail-define-rules
+ ("x"  ?ĉ) ("X"  ?Ĉ) (";x" ?x) (";X" ?X)
+ ("]"  ?ĥ) ("}"  ?Ĥ) (";]" 93) (";}" ?})           
+ ("y"  ?ĝ) ("Y"  ?Ĝ) (";y" ?y) (";Y" ?Y)
+ ("["  ?ĵ) ("{"  ?Ĵ) (";[" 91) (";{" ?{)
+ ("q"  ?ŝ) ("Q"  ?Ŝ) (";q" ?q) (";Q" ?Q)
+ ("w"  ?ŭ) ("W"  ?Ŭ) (";w" ?w) (";W" ?W)
+
+;; Apostrofoj:
+ ("`'" ?ʼ) (";`" ?`)	; letter apostrophe (U+02BC), or
+ (";'" ?’)		; single comma quotation mark (U+2019)
+
+; Punctuation:
+;; Quotation marks:
+ (";<<"  ?«) (";>>"  ?») 		; «foo» or »bar«
+ (";.<"  ?‹) (";.>"  ?›)		; ‹foo› or ›bar‹
+ (";\"<" ?“) (";\">" ?”) (";\"," ?„)	; “foo” or „bar“
+ (";'<"  ?‘) (";'>"  ?’) (";',"  ?‚)	; ‘foo’ or ‚bar‘
+
+ (";  "  ? ) ; NBSP
+ (";-- " ?­) ; SHY
+ (";---" ?—) ; em dash
+ (";--." ?—) ; en dash
+; minuso ‒ ??--------------(FIG, MATH)
+ (";.."  ?…)
+
+ (";!!" ?¡) ; inverted !
+ (";??"  ?¿)
+ (";SO"  ?§) (";PP"  ?¶)
+
+ (";oo" ?°)
+ (";%o" ?‰)  ; per mille
+ (";.-"  ?·) ; cdot
+ (";.="  ?•) ; bullet
+ (";!^"  ?¦) ; broken bar
+
+ (";Oc"  ?©)  ; copy
+ (";Or"  ?®)  ; Registered
+ (";TM"  ?™)  ; Trade Mark
+
+ ;; Currency signs
+ (";B|" ?₿)
+ (";c|" ?¢)
+ (";L-" ?£) ; sterling
+ (";L=" ?₤) ; lira sign
+ (";C=" ?€) (";E=" ?€)
+ (";P-" ?₽) ; ruble
+ (";XO" ?¤)
+ (";Y=" ?¥)
+
+; Extended Latin 
+;; Some ligatures
+ (";AA" ?Å) (";aa" ?å)
+ (";AE" ?Æ) (";ae" ?æ)
+ (";DH" ?Ð) (";dh" ?ð)
+ (";DJ" ?Đ) (";dj" ?đ)
+ (";=H" ?Ħ) (";=h" ?ħ)
+ (";IJ" ?Ĳ) (";ij" ?ĳ) ; Dutch ij
+ (";NG" ?Ŋ) (";ng" ?ŋ)
+ (";OE" ?Œ) (";oe" ?œ)
+ (";fs" ?ſ) ; long s
+ (";ss" ?ß) (";SS" ?ẞ)
+ (";TH" ?Þ) (";th" ?þ)
+
+ (";/O" ?Ø) (";/o" ?ø)
+ (";/L" ?Ł) (";/l" ?ł)
+ (";Lp" ?Ŀ) (";lp" ?ŀ) ; Catalan l dot, as in paraŀlel
+
+ (";.C" ?Ċ) (";.c" ?ċ)
+ (";.E" ?Ė) (";.e" ?ė)
+ (";.G" ?Ġ) (";.g" ?ġ)
+ (";.I" ?İ) (";i" ?ı) ; also \imath
+ (";.Z" ?Ż) (";.z" ?ż)
+
+ (";'A" ?Á) (";'a" ?á)
+ (";'C" ?Ć) (";'c" ?ć)
+ (";'E" ?É) (";'e" ?é)
+ (";'I" ?Í) (";'i" ?í)
+ (";'N" ?Ń) (";'n" ?ń)
+ (";'O" ?Ó) (";'o" ?ó)
+ (";'R" ?Ŕ) (";'r" ?ŕ)
+ (";'S" ?Ś) (";'s" ?ś)
+ (";'U" ?Ú) (";'u" ?ú)
+ (";'Z" ?Ź) (";'z" ?ź)
+
+ (";\"A" ?Ä) (";\"a" ?ä)
+ (";\"E" ?Ë) (";\"e" ?ë)
+ (";\"I" ?Ï) (";\"i" ?ï)
+ (";\"O" ?Ö) (";\"o" ?ö)
+ (";\"U" ?Ü) (";\"u" ?ü)
+
+
+ (";`A" ?À) (";`a" ?à)
+ (";`E" ?È) (";`e" ?è)
+ (";`I" ?Ì) (";`i" ?ì)
+ (";`O" ?Ò) (";`o" ?ò)
+ (";`U" ?Ù) (";`u" ?ù)
+
+ (";^A" ?Â) (";^a" ?â)
+ (";^C" ?Ĉ) (";^c" ?ĉ)
+ (";^E" ?Ê) (";^e" ?ê)
+ (";^G" ?Ĝ) (";^g" ?ĝ)
+ (";^H" ?Ĥ) (";^h" ?ĥ)
+ (";^I" ?Î) (";^i" ?î)
+ (";^J" ?Ĵ) (";^j" ?ĵ)
+ (";^O" ?Ô) (";^o" ?ô)
+ (";^S" ?Ŝ) (";^s" ?ŝ)
+ (";^U" ?Û) (";^u" ?û)
+
+ (";cC" ?Ç) (";cc" ?ç)
+ (";cS" ?Ş) (";cs" ?ş)
+
+ (";=A" ?Ā) (";=a" ?ā)
+ (";=E" ?Ē) (";=e" ?ē)
+ (";=I" ?Ī) (";=i" ?ī)
+ (";=U" ?Ū) (";=u" ?ū)
+
+ (";uA" ?Ă) (";ua" ?ă)
+ (";uG" ?Ğ) (";ug" ?ğ)
+ (";uI" ?Ĭ) (";ui" ?ĭ)
+ (";uO" ?Ŏ) (";uo" ?ŏ)
+ (";uU" ?Ŭ) (";uu" ?ŭ)
+
+ (";vC" ?Č) (";vc" ?č)
+ (";vD" ?Ď) (";vd" ?ď)
+ (";vL" ?Ľ) (";vl" ?ľ)
+ (";vN" ?Ň) (";vn" ?ň)
+ (";vR" ?Ř) (";vr" ?ř)
+ (";vS" ?Š) (";vs" ?š)
+ (";vZ" ?Ž) (";vz" ?ž)
+
+ (";kA" ?Ą) (";ka" ?ą)
+ (";kE" ?Ę) (";ke" ?ę)
+ (";kI" ?Į) (";ki" ?į)
+ (";kU" ?Ų) (";ku" ?ų)
+
+ (";rU" ?Ů) (";ru" ?ů) ; \r[ing] like en EuroTeX
+
+ (";~N" ?Ñ) (";~n" ?ñ)
+ (";~O" ?Õ) (";~o" ?õ)
+
+;; SAMPA:
+ (";\"." ?ˈ) ; primary stress
+ (";%."  ?ˌ) ; secondary stress
+ ; æ ø œ ð ŋ ... are “Ligatures” (which see)
+ (";1" ?ɨ)  ; RU ы
+ (";@" ?ə)  ; schwa
+ (";3" ?ɜ)  ; EN bird
+ (";E." ?ɛ) ; EN bed, FR même
+ (";I." ?ɪ)
+ (";J." ?ɲ) ; ES año, FR oignon
+ (";L." ?ʎ)
+ (";O." ?ɔ) ; EN law, caught
+ (";Q." ?ɒ) ; BrE not, cough
+ (";S." ?ʃ)
+ (";T." ?θ) ; la greka ";theta"
+ (";U." ?ʊ) ; EN put
+ (";V." ?ʌ) ; EN run, enough
+ (";Z." ?ʒ)
+
+; Lowercase Greek letters
+ (";alpha" ?α)
+ (";beta" ?β)
+ (";gamma" ?γ)
+ (";delta" ?δ)
+ (";epsilon" ?ε)
+ (";zeta" ?ζ)
+ (";eta" ?η)
+ (";theta" ?θ) ; ";T." en SAMPA
+ (";vartheta" ?ϑ)
+ (";iota" ?ι)
+ (";kappa" ?κ)
+ (";lambda" ?λ)
+ (";mu" ?μ)
+ (";nu" ?ν)
+ (";ks" ?ξ) ; pok: la TeX'a \xi tro ofte maloportunas (aξs <= axis)
+ (";pi" ?π)
+ (";varpi" ?ϖ)
+ (";rho" ?ρ)
+ (";varrho" ?ϱ)
+ (";sigma" ?σ)
+ (";varsigma" ?ς)
+ (";tau" ?τ)
+ (";upsilon" ?υ)
+ (";phi" ?ϕ)
+ (";varphi" ?φ)
+ (";chi" ?χ)
+ (";psi" ?ψ)
+ (";omega" ?ω)
+
+; Uppercase Greek letters
+ (";Gamma" ?Γ)
+ (";Delta" ?Δ)
+ (";Theta" ?Θ)
+ (";Lambda" ?Λ)
+ (";Ks" ?Ξ) ; pok; vd ;ksi
+ (";Pi" ?Π)
+ (";Sigma" ?Σ)
+ (";Upsilon" ?Υ)
+ (";Phi" ?Φ)
+ (";Psi" ?Ψ)
+ (";Omega" ?Ω)
+
+; Miscellaneous symbols
+ (";aleph" ?ℵ)
+ (";hbar" ?ℏ)
+ (";ell" ?ℓ)
+ (";wp" ?℘)
+ (";Re" ?ℜ)
+ (";Im" ?ℑ)
+ (";lnot" ?¬)
+ (";partial" ?∂)
+ (";88" ?∞)
+ (";prime" ?′)
+ (";emptyset" ?∅)
+ (";nabla" ?∇)
+ (";surd" ?√)
+ (";top" ?⊤)
+ (";bot" ?⊥)
+ (";|" ?∥)
+ (";angle" ?∠)
+ (";triangle" ?△)
+; (";;" ?\;)
+ (";forall" ?∀)
+ (";exists" ?∃)
+ (";flat" ?♭)
+ (";natural" ?♮)
+ (";sharp" ?♯)
+ (";clubsuit" ?♣)
+ (";diamondsuit" ?♢)
+ (";heartsuit" ?♡)
+ (";spadesuit" ?♠)
+ (";mho" ?℧)
+
+; MATH
+;; “Large” operators
+ (";sum" ?∑)
+ (";prod" ?∏)
+ (";coprod" ?∐)
+ (";int" ?∫)
+ (";oint" ?∮)
+ (";bigcap" ?⋂)
+ (";bigcup" ?⋃)
+ (";bigvee" ?⋁)
+ (";bigwedge" ?⋀)
+
+ ;; Binary operations
+ (";+-" ?±)
+ (";-+" ?∓)
+ (";setminus" ?∖)
+ (";xx" ?×) 
+ (";ast" ?∗)
+ (";star" ?⋆)
+ (";diamond" ?⋄)
+ (";circ" ?∘)
+ (";div" ?÷)
+ (";cap" ?∩)
+ (";cup" ?∪)
+ (";uplus" ?⊎)
+ (";sqcap" ?⊓)
+ (";sqcup" ?⊔)
+ (";triangleleft" ?◁)
+ (";triangleright" ?▷)
+ (";wr" ?≀)
+ (";bigcirc" ?◯)
+ (";bigtriangleup" ?△)
+ (";bigtriangledown" ?▽)
+ (";land" ?∧)
+ (";lor" ?∨)
+ (";oplus" ?⊕)
+ (";ominus" ?⊖)
+ (";otimes" ?⊗)
+ (";oslash" ?⊘)
+ (";odot" ?⊙)
+ (";dagger" ?†)
+ (";ddagger" ?‡)
+ (";amalg" ?∐)
+
+ ;; Relations
+
+ (";/=" ?≠)
+ (";<=" ?≤)
+ (";>=" ?≥)
+ (";=_" ?≡)
+ (";~~" ?≈)
+ 
+ (";prec" ?≺)
+ (";preceq" ?≼)
+ (";ll" ?≪)
+ (";subset" ?⊂)
+ (";subseteq" ?⊆)
+ (";sqsubseteq" ?⊑)
+ (";in" ?∈)
+ (";vdash" ?⊢)
+ (";smile" ?⌣)
+ (";frown" ?⌢)
+ (";succ" ?≻)
+ (";succeq" ?≽)
+ (";gg" ?≫)
+ (";supset" ?⊃)
+ (";supseteq" ?⊇)
+ (";sqsupseteq" ?⊒)
+ (";ni" ?∋)
+ (";dashv" ?⊣)
+ (";mid" ?∣)
+ (";parallel" ?∥)
+ (";sim" ?∼)
+ (";simeq" ?≃)
+ (";asymp" ?≍)
+ (";cong" ?≅)
+ (";bowtie" ?⋈)
+ (";propto" ?∝)
+ (";models" ?⊨)
+ (";doteq" ?≐)
+ (";perp" ?⊥)
+
+ ;; Negated relations
+
+ (";not<" ?≮)
+ (";not;leq" ?≰)
+ (";not;prec" ?⊀)
+ (";not;preceq" ?⋠)
+ (";not;subset" ?⊄)
+ (";not;subseteq" ?⊈)
+ (";not;sqsubseteq" ?⋢)
+ (";not>" ?≯)
+ (";not;geq" ?≱)
+ (";not;succ" ?⊁)
+ (";not;succeq" ?⋡)
+ (";not;supset" ?⊅)
+ (";not;supseteq" ?⊉)
+ (";not;sqsupseteq" ?⋣)
+ (";not;equiv" ?≢)
+ (";not;sim" ?≁)
+ (";not;simeq" ?≄)
+ (";not;approx" ?≉)
+ (";not;cong" ?≇)
+ (";not;asymp" ?≭)
+
+ ;; Arrows
+
+ (";<-" ?←)
+ (";=<" ?⇐) ; cf ≤ !!
+ (";->" ?→)
+ (";=>" ?⇒)
+ (";<->" ?↔)
+ (";|^" ?↑) (";|v" ?↓)
+ (";=^" ?⇑) (";=v" ?⇓)
+
+ (";Leftrightarrow" ?⇔)
+ (";mapsto" ?↦)
+ (";hookleftarrow" ?↩)
+ (";leftharpoonup" ?↼)
+ (";leftharpoondown" ?↽)
+ (";rightleftharpoons" ?⇌)
+ (";hookrightarrow" ?↪)
+ (";rightharpoonup" ?⇀)
+ (";rightharpoondown" ?⇁)
+ (";searrow" ?↘)
+ (";swarrow" ?↙)
+ (";nearrow" ?↗)
+ (";nwarrow" ?↖)
+ (";updownarrow" ?↕)
+ (";Updownarrow" ?⇕)
+ (";nearrow" ?↗)
+
+ ;; Openings       ;; Closings    
+                                  
+ (";lfloor" ?⌊)   (";rfloor" ?⌋)
+ (";langle" ?〈)   (";rangle" ?〉)
+ (";lceil" ?⌈)    (";rceil" ?⌉) 
+
+ ;; Alternate names
+
+ (";vert" ?∣)
+ (";Vert" ?∥)
+
+ ;; Non-math symbols
+ (";dag" ?†) (";ddag" ?‡)
+
+ ;; Typographic symbols and ligatures
+
+ (";lq" ?‘) (";rq" ?’) ; pok
+ (";-" ?‐)     ; 2010 (HYPHEN)
+ (";minus" ?−) ; 2212 (MINUS MATH)
+ (";_" ? )     ; pok; A0 (NO-BREAK SPACE)
+ (";!" ?¡) (";?" ?¿) ; as in EuroTeX
+; (";<" ?«) (";>" ?»)
+ (";%" ?‱) ; pok
+ (";0" ?°) ; pok, unlike TeX's \textdegree
+ (";25" ?¼); 0.25, \textonequarter
+ (";5" ?½) ; 0.5, \textonehalf
+ (";75" ?¾); 0.75, \textthreequarters
+
+ ;; Combining characters
+
+ (";'{}" ?́)
+ (";`{}" ?̀)
+ (";^{}" ?̂)
+ (";\"{}" ?̈)
+ (";~{}" ?̃)
+ (";={}" ?̄)
+ (";.{}" ?̇)
+ (";u{}" ?̆)
+ (";v{}" ?̌)
+ (";H{}" ?̋)
+ (";t{}" ?͡)
+ (";c{}" ?̧)
+ (";k{}" ?̨)
+ (";d{}" ?̣)
+ (";b{}" ?̱)
+
+ ;; Subscripts
+
+ ("_0" ?₀)
+ ("_1" ?₁)
+ ("_2" ?₂)
+ ("_3" ?₃)
+ ("_4" ?₄)
+ ("_5" ?₅)
+ ("_6" ?₆)
+ ("_7" ?₇)
+ ("_8" ?₈)
+ ("_9" ?₉)
+ ("_+" ?₊)
+ ("_-" ?₋)
+ ("_=" ?₌)
+ ("_(" ?₍)
+ ("_)" ?₎)
+
+
+ ;; Superscripts
+
+ ("^0" ?⁰)
+ ("^1" ?¹)
+ ("^2" ?²)
+ ("^3" ?³)
+ ("^4" ?⁴)
+ ("^5" ?⁵)
+ ("^6" ?⁶)
+ ("^7" ?⁷)
+ ("^8" ?⁸)
+ ("^9" ?⁹)
+ ("^+" ?⁺)
+ ("^-" ?⁻)
+ ("^=" ?⁼)
+ ("^(" ?⁽)
+ ("^)" ?⁾)
+ ("^a" ?ª) ; pok
+ ("^n" ?ⁿ)
+ ("^o" ?º) ; pok
+
+
+;  ;; \mathbb
+; 
+;  ("|C" ?ℂ)
+;  ("|H" ?ℍ)
+;  ("|N" ?ℕ)
+;  ("|Z" ?ℤ)
+;  ("|P" ?ℙ)
+;  ("|Q" ?ℚ)
+;  ("|R" ?ℝ)
+)
